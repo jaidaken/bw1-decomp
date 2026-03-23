888f67a6a76b1068603b6cafdc0a861bb6b68c68
jaidaken <jamiehewitt@pm.me>
2026-03-23 22:26:10 +0000

    feat: convert SetIdentity from asm to pure C with store_order
    Uses store_order attribute to reorder stores from Clang's ascending
    offset order to MSVC 6.0's descending order. Inline asm constraints
    force constant materialization into EAX register to prevent MOV32mi
    (store immediate) which differs from MSVC's MOV32mr (store register).
    
    MD5: 174b1a64e74b2321f3c38ccc8a511e78

--- src/c/LHMatrix.000.c
+++ src/c/LHMatrix.000.c
@@ -1,25 +1,24 @@
 #include "libs/lionhead/lh3dlib/development/LHMatrix.h"
 
-__attribute__((no_callee_saves, XOR32rr_REV, trailing_asm("call               ?GetVillagerActivityDesire@GameThing@@QAEMPAVVillager@@@Z + 9")))
+__attribute__((no_callee_saves, XOR32rr_REV, store_order("0x2c,0x28,0x24,0x1c,0x18,0x14,0x0c,0x08,0x04,0x20,0x10,0x00"), trailing_asm("call               ?GetVillagerActivityDesire@GameThing@@QAEMPAVVillager@@@Z + 9")))
 void __fastcall SetIdentity__8LHMatrixFv(struct LHMatrix* this)
 {
-    asm volatile (
-        "xor.s              eax, eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x2c], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x28], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x24], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x1c], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x18], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x14], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x0c], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x08], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x04], eax\n\t"
-        "mov                eax, 0x3f800000\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x20], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x10], eax\n\t"
-        "mov                dword ptr [ecx], eax"
-        ::: "eax", "ecx", "edx", "memory"
-    );
+    register unsigned int zero asm("eax");
+    asm volatile("" : "=a"(zero) : "0"(0));
+    unsigned int* p = (unsigned int*)this->m;
+    p[11] = zero;
+    p[10] = zero;
+    p[9] = zero;
+    p[7] = zero;
+    p[6] = zero;
+    p[5] = zero;
+    p[3] = zero;
+    p[2] = zero;
+    p[1] = zero;
+    asm volatile("" : "=a"(zero) : "0"(0x3f800000));
+    p[8] = zero;
+    p[4] = zero;
+    p[0] = zero;
 }
 
 __attribute__((XOR32rr_REV))

a79557c2efae8a0bdcf3fb86a8a9de633b9a7874
jaidaken <jamiehewitt@pm.me>
2026-03-24 13:03:29 +0000

    feat: convert IsWonder (Abode) with msvc6_regswap
    Same pattern as IsFemaleVillager: pointer-chain + non-zero CMP.
    
    MD5: 174b1a64e74b2321f3c38ccc8a511e78

--- src/c/Abode.007.c
+++ src/c/Abode.007.c
@@ -1,6 +1,6 @@
 #include "Abode.h"
 
-__attribute__((msvc6_regalloc, prefer_sete_ecx))
+__attribute__((msvc6_regswap, unfold_cmp_mem_ecx, expand_movzx, XOR32rr_REV))
 bool32_t __fastcall IsWonder__5AbodeFv(struct GameThingWithPos* this)
 {
     return *(uint32_t*)((char*)*(void**)((char*)this + 0x28) + 0x120) == 0x100;

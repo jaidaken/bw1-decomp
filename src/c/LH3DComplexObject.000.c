74335a59247bdbf01a5a9e83cfbab164bd6aec44
jaidaken <jamiehewitt@pm.me>
2026-03-26 23:49:32 +0000

    feat: wrap 10 LH3DComplexObject accessor functions as C asm volatile
    Move 10 simple getter/setter functions (0x0080b870-0x0080b90f) from
    the unprocessed asm file into a new C source file. These are virtual
    function overrides for a class at vtable 0x009a3424, implementing
    field accessors for matrix_0x80, field_0x84, hair, field_0x8c, and
    field_0x90 of LH3DComplexObject.
    
    Each function is wrapped in asm volatile with no_callee_saves and
    trailing_asm for nop padding to maintain byte-exact binary output.
    
    MD5 verified: 174b1a64e74b2321f3c38ccc8a511e78

---
+++ src/c/LH3DComplexObject.000.c
@@ -0,0 +1,114 @@
+// LH3DComplexObject virtual function overrides: simple getters/setters
+// Address range: 0x0080b870 - 0x0080b90f
+
+#include "libs/lionhead/lh3dlib/development/LH3DComplexObject.h"
+
+// 0x0080b870: mov eax, [ecx + 0x80]; ret (+ 9 nops)
+__attribute__((no_callee_saves, trailing_asm("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop")))
+void* __fastcall FUN_0080b870(struct LH3DComplexObject* this)
+{
+    void* result;
+    asm volatile (
+        "%{disp32%} mov eax, dword ptr [ecx + 0x00000080]"
+        : "=a"(result) : "c"(this) : "memory"
+    );
+    return result;
+}
+
+// 0x0080b880: mov [ecx + 0x84], edx; ret (+ 9 nops)
+__attribute__((no_callee_saves, trailing_asm("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop")))
+void __fastcall FUN_0080b880(struct LH3DComplexObject* this, uint32_t* value)
+{
+    asm volatile (
+        "%{disp32%} mov dword ptr [ecx + 0x00000084], edx"
+        :: "c"(this), "d"(value) : "memory"
+    );
+}
+
+// 0x0080b890: mov eax, [ecx + 0x84]; ret (+ 9 nops)
+__attribute__((no_callee_saves, trailing_asm("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop")))
+uint32_t* __fastcall FUN_0080b890(struct LH3DComplexObject* this)
+{
+    uint32_t* result;
+    asm volatile (
+        "%{disp32%} mov eax, dword ptr [ecx + 0x00000084]"
+        : "=a"(result) : "c"(this) : "memory"
+    );
+    return result;
+}
+
+// 0x0080b8a0: mov [ecx + 0x88], edx; ret (+ 9 nops)
+__attribute__((no_callee_saves, trailing_asm("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop")))
+void __fastcall FUN_0080b8a0(struct LH3DComplexObject* this, void* value)
+{
+    asm volatile (
+        "%{disp32%} mov dword ptr [ecx + 0x00000088], edx"
+        :: "c"(this), "d"(value) : "memory"
+    );
+}
+
+// 0x0080b8b0: mov eax, [ecx + 0x88]; ret (+ 9 nops)
+__attribute__((no_callee_saves, trailing_asm("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop")))
+void* __fastcall FUN_0080b8b0(struct LH3DComplexObject* this)
+{
+    void* result;
+    asm volatile (
+        "%{disp32%} mov eax, dword ptr [ecx + 0x00000088]"
+        : "=a"(result) : "c"(this) : "memory"
+    );
+    return result;
+}
+
+// 0x0080b8c0: mov [ecx + 0x8c], edx; ret (+ 9 nops)
+__attribute__((no_callee_saves, trailing_asm("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop")))
+void __fastcall FUN_0080b8c0(struct LH3DComplexObject* this, uint32_t value)
+{
+    asm volatile (
+        "%{disp32%} mov dword ptr [ecx + 0x0000008c], edx"
+        :: "c"(this), "d"(value) : "memory"
+    );
+}
+
+// 0x0080b8d0: mov eax, [ecx + 0x8c]; ret (+ 9 nops)
+__attribute__((no_callee_saves, trailing_asm("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop")))
+uint32_t __fastcall FUN_0080b8d0(struct LH3DComplexObject* this)
+{
+    uint32_t result;
+    asm volatile (
+        "%{disp32%} mov eax, dword ptr [ecx + 0x0000008c]"
+        : "=a"(result) : "c"(this) : "memory"
+    );
+    return result;
+}
+
+// 0x0080b8e0: mov eax, [esp+4]; mov [ecx + 0x90], eax; ret 4 (+ 3 nops)
+__attribute__((no_callee_saves, no_ret, trailing_asm("nop\nnop\nnop")))
+void __attribute__((thiscall)) FUN_0080b8e0(struct LH3DComplexObject* this, float value)
+{
+    asm volatile (
+        "%{disp8%} mov eax, dword ptr [esp + 0x04]\n\t"
+        "%{disp32%} mov dword ptr [ecx + 0x00000090], eax\n\t"
+        "ret 0x0004"
+        :: "c"(this) : "eax", "memory"
+    );
+    __builtin_unreachable();
+}
+
+// 0x0080b8f0: fld [ecx + 0x90]; ret (+ 9 nops)
+__attribute__((no_callee_saves, trailing_asm("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop")))
+float __fastcall FUN_0080b8f0(struct LH3DComplexObject* this)
+{
+    float result;
+    asm volatile (
+        "%{disp32%} fld dword ptr [ecx + 0x00000090]"
+        : "=t"(result) : "c"(this) : "memory"
+    );
+    return result;
+}
+
+// 0x0080b900: mov eax, 1; ret (+ 10 nops)
+__attribute__((no_callee_saves, trailing_asm("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop")))
+uint32_t __fastcall FUN_0080b900(struct LH3DComplexObject* this)
+{
+    return 1;
+}

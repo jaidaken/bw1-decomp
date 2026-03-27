de73f1c96f482309bf83c18974d58eb13cf776ee
jaidaken <jamiehewitt@pm.me>
2026-03-27 21:49:29 +0000

    feat: convert LHOSFile destructor to pure C
    force_this_esi + forced_callee_saves(esi) for ESI save/restore.
    dllimport CloseHandle generates correct IAT call (ff 15).
    MD5 verified: 174b1a64e74b2321f3c38ccc8a511e78

--- src/c/LHOSFile.000.c
+++ src/c/LHOSFile.000.c
@@ -22,23 +22,16 @@ void __dt__8LHOSFileFb_decl(void) {}
 
 // win1.41 007bc680 mac inlined LHOSFile::~LHOSFile(void)
 // Bytes: 56 8bf1 8b4604 c7066cf89900 85c0 740e 50 ff15c4928a00 c7460400000000 5e c3
-void __dt__8LHOSFileFv_impl(void) __asm__("??_DLHOSFile@@QAEXXZ");
-__attribute__((no_callee_saves, no_ret))
-void __dt__8LHOSFileFv_impl(void)
+void __fastcall __dt__8LHOSFileFv_impl(struct LHOSFile* this) __asm__("??_DLHOSFile@@QAEXXZ");
+__attribute__((forced_callee_saves("esi"), force_this_esi, MOV32rr_REV))
+void __fastcall __dt__8LHOSFileFv_impl(struct LHOSFile* this)
 {
-    asm volatile (
-        "push               esi\n\t"
-        "mov.s              esi, ecx\n\t"
-        "%{disp8%} mov      eax, dword ptr [esi + 0x04]\n\t"
-        "mov                dword ptr [esi], offset rdata_bytes + 0xf686c\n\t"
-        "test               eax, eax\n\t"
-        "%{disp8%} je       LAB__dt_skip\n\t"
-        "push               eax\n\t"
-        "call               dword ptr [__imp__CloseHandle@4]\n\t"
-        "%{disp8%} mov      dword ptr [esi + 0x04], 0x00000000\n"
-        "LAB__dt_skip:\n\t"
-        "pop                esi\n\t"
-        "ret"
-        ::: "eax", "esi", "memory"
-    );
+    extern __attribute__((dllimport)) int __stdcall CloseHandle(void*);
+
+    void* handle = *(void**)((char*)this + 0x04);
+    *(uint32_t*)((char*)this + 0x00) = (uint32_t)(rdata_bytes + 0xf686c);
+    if (handle) {
+        CloseHandle(handle);
+        *(uint32_t*)((char*)this + 0x04) = 0;
+    }
 }

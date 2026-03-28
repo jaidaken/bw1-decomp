1d4614245d1d7ae3d2a05cb66911f821a7a44154
jaidaken <jamiehewitt@pm.me>
2026-03-28 19:31:45 +0000

    feat: replace asm volatile register hints with prefer_register_store
    Remove 4 asm volatile blocks from SetIdentity and Translation that
    forced EAX register materialization for zero and 1.0f stores. The new
    prefer_register_store LLVM pass handles this automatically, converting
    repeated MOV32mi immediate stores into XOR+MOV32mr or MOV32ri+MOV32mr.
    
    MD5: 174b1a64e74b2321f3c38ccc8a511e78

--- src/c/LHMatrix.000.c
+++ src/c/LHMatrix.000.c
@@ -1,45 +1,39 @@
 #include "libs/lionhead/lh3dlib/development/LHMatrix.h"
 
-__attribute__((no_callee_saves, XOR32rr_REV, store_order("0x2c,0x28,0x24,0x1c,0x18,0x14,0x0c,0x08,0x04,0x20,0x10,0x00"), trailing_asm("call               ?GetVillagerActivityDesire@GameThing@@QAEMPAVVillager@@@Z + 9")))
+__attribute__((no_callee_saves, XOR32rr_REV, prefer_register_store, store_order("0x2c,0x28,0x24,0x1c,0x18,0x14,0x0c,0x08,0x04,0x20,0x10,0x00"), trailing_asm("call               ?GetVillagerActivityDesire@GameThing@@QAEMPAVVillager@@@Z + 9")))
 void __fastcall SetIdentity__8LHMatrixFv(struct LHMatrix* this)
 {
-    register unsigned int zero asm("eax");
-    asm volatile("" : "=a"(zero) : "0"(0));
     unsigned int* p = (unsigned int*)this->m;
-    p[11] = zero;
-    p[10] = zero;
-    p[9] = zero;
-    p[7] = zero;
-    p[6] = zero;
-    p[5] = zero;
-    p[3] = zero;
-    p[2] = zero;
-    p[1] = zero;
-    asm volatile("" : "=a"(zero) : "0"(0x3f800000));
-    p[8] = zero;
-    p[4] = zero;
-    p[0] = zero;
+    p[11] = 0;
+    p[10] = 0;
+    p[9] = 0;
+    p[7] = 0;
+    p[6] = 0;
+    p[5] = 0;
+    p[3] = 0;
+    p[2] = 0;
+    p[1] = 0;
+    p[8] = 0x3f800000;
+    p[4] = 0x3f800000;
+    p[0] = 0x3f800000;
 }
 
-__attribute__((XOR32rr_REV, no_callee_saves, store_order("0x2c,0x28,0x24,0x1c,0x18,0x14,0x0c,0x08,0x04,0x20,0x10,0x00")))
+__attribute__((XOR32rr_REV, no_callee_saves, prefer_register_store, store_order("0x2c,0x28,0x24,0x1c,0x18,0x14,0x0c,0x08,0x04,0x20,0x10,0x00")))
 void __fastcall Translation__8LHMatrixFv(struct LHMatrix* this, const struct LHPoint* translation)
 {
-    register unsigned int zero asm("eax");
-    asm volatile("" : "=a"(zero) : "0"(0));
     unsigned int* p = (unsigned int*)this->m;
-    p[11] = zero;
-    p[10] = zero;
-    p[9] = zero;
-    p[7] = zero;
-    p[6] = zero;
-    p[5] = zero;
-    p[3] = zero;
-    p[2] = zero;
-    p[1] = zero;
-    asm volatile("" : "=a"(zero) : "0"(0x3f800000));
-    p[8] = zero;
-    p[4] = zero;
-    p[0] = zero;
+    p[11] = 0;
+    p[10] = 0;
+    p[9] = 0;
+    p[7] = 0;
+    p[6] = 0;
+    p[5] = 0;
+    p[3] = 0;
+    p[2] = 0;
+    p[1] = 0;
+    p[8] = 0x3f800000;
+    p[4] = 0x3f800000;
+    p[0] = 0x3f800000;
     *(unsigned int*)((char*)this + 0x24) = *(const unsigned int*)((const char*)translation + 0x00);
     *(unsigned int*)((char*)this + 0x28) = *(const unsigned int*)((const char*)translation + 0x04);
     {

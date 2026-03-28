8ace7aa36383d5f38c3f1fe9ff2949c38829f85e
jaidaken <jamiehewitt@pm.me>
2026-03-28 21:01:27 +0000

    feat: convert Translation__8LHMatrixFv to pure C
    Remove EDX register hint asm volatile block using prefer_source_register_reuse
    attribute. Function is now fully expressed in C.

--- src/c/LHMatrix.000.c
+++ src/c/LHMatrix.000.c
@@ -18,7 +18,7 @@ void __fastcall SetIdentity__8LHMatrixFv(struct LHMatrix* this)
     p[0] = 0x3f800000;
 }
 
-__attribute__((XOR32rr_REV, no_callee_saves, prefer_register_store, store_order("0x2c,0x28,0x24,0x1c,0x18,0x14,0x0c,0x08,0x04,0x20,0x10,0x00")))
+__attribute__((XOR32rr_REV, no_callee_saves, prefer_register_store, prefer_source_register_reuse, store_order("0x2c,0x28,0x24,0x1c,0x18,0x14,0x0c,0x08,0x04,0x20,0x10,0x00")))
 void __fastcall Translation__8LHMatrixFv(struct LHMatrix* this, const struct LHPoint* translation)
 {
     unsigned int* p = (unsigned int*)this->m;
@@ -36,11 +36,7 @@ void __fastcall Translation__8LHMatrixFv(struct LHMatrix* this, const struct LHP
     p[0] = 0x3f800000;
     *(unsigned int*)((char*)this + 0x24) = *(const unsigned int*)((const char*)translation + 0x00);
     *(unsigned int*)((char*)this + 0x28) = *(const unsigned int*)((const char*)translation + 0x04);
-    {
-        register unsigned int last asm("edx");
-        asm volatile("" : "=d"(last) : "0"(*(const unsigned int*)((const char*)translation + 0x08)));
-        *(unsigned int*)((char*)this + 0x2c) = last;
-    }
+    *(unsigned int*)((char*)this + 0x2c) = *(const unsigned int*)((const char*)translation + 0x08);
 }
 
 __attribute__((no_callee_saves, prefer_fmul_mem, trailing_asm("call               ?GetVillagerActivityDesire@GameThing@@QAEMPAVVillager@@@Z + 9")))

454ea338472cd05e73387e4b62183d5e966503c5
jaidaken <jamiehewitt@pm.me>
2026-03-28 15:02:52 +0000

    feat: convert LHMatrix::Translation to C
    Uses store_order for identity-set portion + uint32_t copies for
    translation fields with asm volatile EDX hint for last copy.
    
    Attributes: XOR32rr_REV, no_callee_saves, store_order

--- src/c/LHMatrix.000.c
+++ src/c/LHMatrix.000.c
@@ -21,32 +21,32 @@ void __fastcall SetIdentity__8LHMatrixFv(struct LHMatrix* this)
     p[0] = zero;
 }
 
-__attribute__((XOR32rr_REV))
+__attribute__((XOR32rr_REV, no_callee_saves, store_order("0x2c,0x28,0x24,0x1c,0x18,0x14,0x0c,0x08,0x04,0x20,0x10,0x00")))
 void __fastcall Translation__8LHMatrixFv(struct LHMatrix* this, const struct LHPoint* translation)
 {
-    asm volatile (
-        "xor.s              eax, eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x2c], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x28], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x24], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x1c], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x18], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x14], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x0c], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x08], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x04], eax\n\t"
-        "mov                eax, 0x3f800000\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x20], eax\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x10], eax\n\t"
-        "mov                dword ptr [ecx], eax\n\t"
-        "mov                eax, dword ptr [edx]\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x24], eax\n\t"
-        "%{disp8%} mov        eax, dword ptr [edx + 0x04]\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x28], eax\n\t"
-        "%{disp8%} mov        edx, dword ptr [edx + 0x08]\n\t"
-        "%{disp8%} mov        dword ptr [ecx + 0x2c], edx"
-        :  : "c"(this) : "eax", "edx", "memory"
-    );
+    register unsigned int zero asm("eax");
+    asm volatile("" : "=a"(zero) : "0"(0));
+    unsigned int* p = (unsigned int*)this->m;
+    p[11] = zero;
+    p[10] = zero;
+    p[9] = zero;
+    p[7] = zero;
+    p[6] = zero;
+    p[5] = zero;
+    p[3] = zero;
+    p[2] = zero;
+    p[1] = zero;
+    asm volatile("" : "=a"(zero) : "0"(0x3f800000));
+    p[8] = zero;
+    p[4] = zero;
+    p[0] = zero;
+    *(unsigned int*)((char*)this + 0x24) = *(const unsigned int*)((const char*)translation + 0x00);
+    *(unsigned int*)((char*)this + 0x28) = *(const unsigned int*)((const char*)translation + 0x04);
+    {
+        register unsigned int last asm("edx");
+        asm volatile("" : "=d"(last) : "0"(*(const unsigned int*)((const char*)translation + 0x08)));
+        *(unsigned int*)((char*)this + 0x2c) = last;
+    }
 }
 
 __attribute__((no_callee_saves, prefer_fmul_mem, trailing_asm("call               ?GetVillagerActivityDesire@GameThing@@QAEMPAVVillager@@@Z + 9")))
